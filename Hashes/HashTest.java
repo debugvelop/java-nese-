262fd9538aa0317486a2f5ee625943c51386cc8c
Debug Develop <debugvelop@gmail.com>
2019-04-23 22:43:49 +0700

    Put Hashes class outside HashTest class

--- Hashes/HashTest.java
+++ Hashes/HashTest.java
@@ -3,62 +3,62 @@ import java.util.*;
 public class HashTest{
     public static void main(String[] args){
         int counter=1;
-        Hashes fooHash=new Hashes(100);
+        Hashes fooHash=new Hashes(64);
         fooHash.showHash();
-        while(counter<=95){
+        while(counter<=56){
             fooHash.hashIn(counter*4);
             ++counter;
         }
         fooHash.showHash();
     }
+}
 
-    static class Hashes extends HashTest{
-        private int[] hashTable;
-        private int length;
+class Hashes extends HashTest{
+    private int[] hashTable;
+    private int length;
 
-        public Hashes(int length){
-            this.length=length;
-            hashTable=new int[length];
+    public Hashes(int length){
+        this.length=length;
+        hashTable=new int[length];
+    }
+
+    public void showHash(){
+        for(int counter=0;counter<length;counter++){
+            System.out.print(hashTable[counter]+" ");
         }
+        System.out.println();
+    }
 
-        public void showHash(){
-            for(int counter=0;counter<length;counter++){
-                System.out.print(hashTable[counter]+" ");
-            }
-            System.out.println();
+    public void hashIn(int input){
+        if(hashTable[input%53]==0){
+            hashTable[input%53]=input;
         }
+        else{
+            collideHandle(input,input%53);
+        }
+    }
 
-        public void hashIn(int input){
-            if(hashTable[input%53]==0){
-                hashTable[input%53]=input;
+    public void collideHandle(int input,int pos){
+        int index=1;
+        boolean stop=false;
+        while(!stop){
+            int nextIn=pos+(int)Math.pow(index,2);
+            if(nextIn<length-1){
+                if(hashTable[nextIn]==0){
+                    hashTable[nextIn]=input;
+                    stop=true;
+                }
             }
             else{
-                collideHandle(input,input%53);
-            }
-        }
-
-        public void collideHandle(int input,int pos){
-            int index=1;
-            boolean stop=false;
-            while(!stop){
-                int nextIn=pos+(int)Math.pow(index,2);
-                if(nextIn<length-1){
-                    if(hashTable[nextIn]==0){
-                        hashTable[nextIn]=input;
-                        stop=true;
-                    }
+                while(nextIn>length-1){
+                    nextIn=(nextIn-1)-(length-1);
                 }
-                else{
-                    while(nextIn>length-1){
-                        nextIn=(nextIn-1)-(length-1);
-                    }
-                    if(hashTable[nextIn]==0){
-                        hashTable[nextIn]=input;
-                        stop=true;
-                    }
+                if(hashTable[nextIn]==0){
+                    hashTable[nextIn]=input;
+                    stop=true;
                 }
-                ++index;
             }
+            ++index;
         }
     }
 }
